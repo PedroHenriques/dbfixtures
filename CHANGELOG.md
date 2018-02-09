8506de729db65d37c07e08e3336f886c1744452f
PedroHenriques <pedro@pedrojhenriques.com>
2018-02-09 15:43:41 +0000

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
+## [1.0.1] - 2018-02-09
+### Fixed
+- Type definitions for IFixtures and IDriver, replacing `[{}]` with `{}[]`.
+- Added missing package-lock.json file.
+
 ## [1.0.0] - 2018-01-20
 ### Added
-
 - First version of the code
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,10 +2,10 @@
 
 export interface IDriver {
   truncate: (tableNames: string[]) => Promise<void>,
-  insertFixtures: (tableName: string, fixtures: [{}]) => Promise<void>,
+  insertFixtures: (tableName: string, fixtures: {}[]) => Promise<void>,
   close: () => Promise<void>,
 }
 
 export interface IFixtures {
-  [key: string]: [{}]
+  [key: string]: {}[],
 }
